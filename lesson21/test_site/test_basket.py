ec4c24166705e35f552637260e152ea1d06798c7
Anastasiia Prokopenko <nastia.prokopenko20@gmail.com>
2024-03-16 19:03:18 +0200

    Homework for lesson 18. Pytest

--- lesson21/test_site/test_basket.py
+++ lesson21/test_site/test_basket.py
@@ -1,3 +1,5 @@
+from lesson21.pages.product_page import ProductPage
+
 
 def test_click_buy_and_check_product(product):
     basket_page = product.on_product_page()
@@ -16,9 +18,13 @@ def test_click_cross_button(product):
     basket_page = product.on_product_page()
     assert basket_page.this_page()
     basket_page.click_cross()
+    product_page = ProductPage(product.driver)
+    assert product_page.this_page()
 
 
 def test_click_back_button(product):
     basket_page = product.on_product_page()
     assert basket_page.this_page()
     basket_page.click_back()
+    product_page = ProductPage(product.driver)
+    assert product_page.this_page()
